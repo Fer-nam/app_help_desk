2d674a0c63dd2d02da107cca4b08d1abfb131d70
Fer-nam <fernandosilva348@gmail.com>
2022-07-03 15:23:58 -0300

    Primeiros passos

---
+++ valida_login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*
+    print_r($_GET);
+
+    echo '<br>';
+
+    echo $_GET['email'];
+    echo '<br>';
+    echo $_GET['senha'];
+   */
+
+   print_r($_POST );
+
+   echo '<br>';
+
+   echo $_POST['email'];
+   echo '<br>';
+   echo $_POST['senha'];
+
+
+    ?>
+</body>
+</html>
