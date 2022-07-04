6cec091049978a73a235ab1f7b2af95a66ae6ba1
Fer-nam <fernandosilva348@gmail.com>
2022-07-04 19:47:27 -0300

    registrando chamados

---
+++ registra_chamado.php
@@ -0,0 +1,29 @@
+<?php
+
+    ECHO '<PRE>';
+    print_r($_POST);
+    echo '</pre>';
+
+    //estamos trablhando na montagem do ovo
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    $texto = $titulo .'#'. $categoria .'#'. $descricao.PHP_EOL;
+
+    //PHP_EOL = quebra a linha do arquivo de texto
+
+    //Função para abrir um arquivo no php
+    $arquivo = fopen('arquivo.hd','a');
+
+   
+
+    //Função para escrever um texto no arquivo aberto
+    fwrite($arquivo, $texto);
+
+
+    //Função para fechar o arquivo 
+    fclose($arquivo);
+
+    //echo $texto;
+header('Location: abrir_chamado.php');
